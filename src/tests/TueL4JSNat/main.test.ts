bd0d8870bce3ef3bc959024aaafb3e6123790df8
para_slov <paraslovjm@gmail.com>
2021-04-30 07:22:34 +0300

    L.JSNat.Tue.W4 Practice is done.

--- src/tests/TueL4JSNat/main.test.ts
+++ src/tests/TueL4JSNat/main.test.ts
@@ -1,4 +1,12 @@
-import {numsSum, sum, triangleType} from './main';
+import {
+    getBanknoteList,
+    getBanknoteListAssociateArray,
+    isSquareGreater,
+    numsSum,
+    sum,
+    sumEvenOdd,
+    triangleType
+} from './main';
 
 test('func sum should return sum off all its parameters', () => {
     let result = sum(2, 4, 6, -4)
@@ -21,4 +29,46 @@ test('sum of number numbers', () => {
     expect(numsSum(123)).toBe(6)
     expect(numsSum(15233)).toBe(14)
     expect(numsSum(764)).toBe(17)
+})
+
+test('sum of even index numbers in arr should be greater than odd', () => {
+    expect(sumEvenOdd([2, 4, 2, -3, 10, 12])).toBe(true)
+    expect(sumEvenOdd([2, 4, 2, -3, 10, 15])).toBe(false)
+    expect(sumEvenOdd([-200, 300, 500, 637, 78, 25, -57, 64, 89, 0])).toBe(false)
+    expect(sumEvenOdd([2, 4, 2, -3, 10, 13])).toBe('equal')
+})
+
+test('should return true if circle can be placed in square', () => {
+    expect(isSquareGreater(4, 3.14)).toBe(true)
+    expect(isSquareGreater(100, 100)).toBe(false)
+    expect(isSquareGreater(144, 100)).toBe(true)
+    expect(isSquareGreater(144, 300)).toBe(false)
+    expect(isSquareGreater(-144, 100)).toBe('Error: areas should be more than zero')
+    expect(isSquareGreater(144, -100)).toBe('Error: areas should be more than zero')
+})
+
+test('banknote list', () => {
+    let result1500 = getBanknoteList(1500)
+    let result23 = getBanknoteList(23)
+    let result26439 = getBanknoteList(26439)
+    let result38788 = getBanknoteListAssociateArray(38788)
+    let result1253914 = getBanknoteListAssociateArray(1253914)
+
+    console.log('38788: ', result38788)
+    console.log('1253914', result1253914)
+
+    expect(result1500[0]).toBe(1000)
+    expect(result1500[1]).toBe(500)
+    expect(result23[0]).toBe(20)
+    expect(result23[1]).toBe(2)
+    expect(result23[2]).toBe(1)
+    expect(result26439).toStrictEqual([
+        5000, 5000, 5000, 5000, 5000,
+        1000,
+        100, 100, 100, 100,
+        20,
+        10,
+        5,
+        2, 2
+    ])
 })
--- src/tests/TueL4JSNat/main.ts
+++ src/tests/TueL4JSNat/main.ts
@@ -1,5 +1,3 @@
-
-
 export function sum(...numbers: number[]) {
     return numbers.reduce((acc, num) => acc + num)
 }
@@ -9,20 +7,133 @@ export function triangleType(a: number, b: number, c: number) {
     if (sum(a, b, c) - maxSide <= maxSide) {
         return 'incorrect'
     }
-    if(a === b && b === c) {
+    if (a === b && b === c) {
         return 'same sides'
     } else if (a === b || b === c || c === a) {
         return 'same hips'
     } else if (sum(a, b, c) - maxSide > maxSide) {
         return 'regular'
-    }  else {
+    } else {
         return 'something goes wrong'
     }
 }
 
-export function numsSum(num: number){
+export function numsSum(num: number) {
     return num
         .toString()
         .split('')
-        .reduce((acc, num) => acc+(+num), 0)
+        .reduce((acc, num) => acc + (+num), 0)
 }
+
+export function sumEvenOdd(arr: number[]) {
+    let sumEven: number = 0
+    let sumOdd: number = 0
+    arr.forEach((el, i) => i % 2 === 0 ? sumEven += el : sumOdd += el)
+    if (sumEven > sumOdd) return true
+    else if (sumEven < sumOdd) return false
+    else if (sumEven === sumOdd) return 'equal'
+    return 'something goes wrong'
+}
+
+export function isSquareGreater(square: number, circle: number) {
+    if (square <= 0 || circle <= 0) {
+        return 'Error: areas should be more than zero'
+    }
+    let squareSide = Math.sqrt(square)
+    let radiusCircle = Math.sqrt(circle / Math.PI)
+    if (squareSide === radiusCircle * 2) return true
+    return squareSide / 2 > radiusCircle
+}
+
+// const banknotes = [5000, 1000,500,100,50,20,10,5,2,1]
+
+export function getBanknoteList(moneyAmount: number) {
+    let arr: number[] = []
+    while (moneyAmount > 0) {
+        if (moneyAmount >= 5000) {
+            arr.push(5000)
+            moneyAmount -= 5000
+            continue
+        }
+        if (moneyAmount >= 1000) {
+            arr.push(1000)
+            moneyAmount -= 1000
+        } else if (moneyAmount < 1000 && moneyAmount >= 500) {
+            arr.push(500)
+            moneyAmount -= 500
+        } else if (moneyAmount < 500 && moneyAmount >= 100) {
+            arr.push(100)
+            moneyAmount -= 100
+        } else if (moneyAmount < 100 && moneyAmount >= 50) {
+            arr.push(50)
+            moneyAmount -= 50
+        } else if (moneyAmount < 50 && moneyAmount >= 20) {
+            arr.push(20)
+            moneyAmount -= 20
+        } else if (moneyAmount < 20 && moneyAmount >= 10) {
+            arr.push(10)
+            moneyAmount -= 10
+        } else if (moneyAmount < 10 && moneyAmount >= 5) {
+            arr.push(5)
+            moneyAmount -= 5
+        } else if (moneyAmount < 5 && moneyAmount >= 2) {
+            arr.push(2)
+            moneyAmount -= 2
+        } else if (moneyAmount === 1) {
+            arr.push(1)
+            moneyAmount = 0
+        }
+    }
+    return arr
+}
+
+// const banknotes = [5000, 1000,500,100,50,10,5,2,1]
+
+export function getBanknoteListAssociateArray(moneyAmount: number) {
+    let arr = {
+        ['5000']: 0,
+        ['1000']: 0,
+        ['500']: 0,
+        ['100']: 0,
+        ['50']: 0,
+        ['10']: 0,
+        ['5']: 0,
+        ['2']: 0,
+        ['1']: 0,
+    }
+
+    while (moneyAmount > 0) {
+        if (moneyAmount >= 5000) {
+            arr['5000'] += 1
+            moneyAmount -= 5000
+            continue
+        }
+        if (moneyAmount >= 1000) {
+            arr['1000'] += 1
+            moneyAmount -= 1000
+        } else if (moneyAmount < 1000 && moneyAmount >= 500) {
+            arr['500'] += 1
+            moneyAmount -= 500
+        } else if (moneyAmount < 500 && moneyAmount >= 100) {
+            arr['100'] += 1
+            moneyAmount -= 100
+        } else if (moneyAmount < 100 && moneyAmount >= 50) {
+            arr['50'] += 1
+            moneyAmount -= 50
+        } else if (moneyAmount < 50 && moneyAmount >= 10) {
+            arr['10'] += 1
+            moneyAmount -= 10
+        } else if (moneyAmount < 10 && moneyAmount >= 5) {
+            arr['5'] += 1
+            moneyAmount -= 5
+        } else if (moneyAmount < 5 && moneyAmount >= 2) {
+            arr['2'] += 1
+            moneyAmount -= 2
+        } else if (moneyAmount === 1) {
+            arr['1'] += 1
+            moneyAmount = 0
+        }
+    }
+    return arr
+}
+
